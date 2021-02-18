d7a86ad4d67f01140e21471ad523a19850864dc0
emhedlin <hedlin@ualberta.ca>
2021-02-18 11:06:01 -0600

    starting occupancy to get a handle on Qam community analysis

--- scripts.jl/chapter_13_3.jl
+++ scripts.jl/chapter_13_3.jl
@@ -1,4 +1,4 @@
-using Distributions, Random, Turing, DataFrames, Distributed, Plots, MCMCChains, StatsPlots, CSV
+using Distributions, Random, Turing, DataFrames, Distributed, Plots, MCMCChains, StatsPlots, CSV, SpecialFunctions
 
 #= 
 What Happens when p < 1 and constant, and p is not accounted for in a species distribution model
@@ -41,7 +41,49 @@ sum(y)
         end
     end
 
+    
     # Likelihood ~~~~~~~
     for i in 1:n_sites
-        if sum_y
+        if sum_y[i] < 0
+            1 ~ Bernoulli(psi)
+            y[i] ~ Bernoulli(p)
+        else
+            target += log(sum(exp()))
+        end
+    end
 end
+
+
+n_sites = size(y)[1]
+n_time = size(y)[2]
+sum_y = zeros(n_sites)
+
+
+#= 
+The true state of site i, occupied or not, is represented by the 
+parameter in our model "z". This is a discrete parameter, either 1 or 0,
+which has to be marginalized out to allow HMC sampling. In general,
+if you have a joint distribution for y (observation dependent on z and p), 
+and z (true state dependent on ψ), we obtain the marginal distribution of y by
+summing the joint distribution over all possible values of z, which in This
+case is 1 or 0:
+
+marginalized distribution:
+                  
+                 |log(ψ) + log(Binomial(yi | p))                         1. for yi > 0 
+log[y_i | p,ψ] = |
+                 |log(e^log(ψ) + log(Binomial(yi | p)) + e^log(1-ψ))     2. for yi = 0
+
+1.
+2. log(exp( log(ψ) + log(Binomial(yi|p) ) + exp( log(1-ψ)) ))
+
+
+
+
+=#
+
+
+
+
+x = 1:100
+plot(loggamma.(x))
