ef47e187f7103cf5593a6d0c94f5dc0c4d01f798
emhedlin <hedlin@ualberta.ca>
2021-05-02 22:58:29 -0600

    Update plot_utils.jl

--- src/plot_utils.jl
+++ src/plot_utils.jl
@@ -7,6 +7,6 @@ grey1 = colorant"#D4D4D4";
 grey2 = colorant"#F4F4F4";
 
 defs1 = (markercolor = grey1, legend = false, markerstrokewidth = 0, markersize = 2, grid = false, showaxis = false, tickfontcolor = grey1)
-defs2 = (markercolor = primary, legend = false, markerstrokewidth = 0, markersize = 2, grid = false, showaxis = false, tickfontcolor = grey1)
+defs2 = (markercolor = primary, legend = false, markerstrokewidth = 0, markersize = 2, grid = false, showaxis = false, tickfontcolor = grey2)
 
 
